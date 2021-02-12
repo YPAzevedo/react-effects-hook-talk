92f3a28b2fe10ab43e8868f7a39a1ebe8d2b8b03
Yago Pereira Azevedo <56167866+YPAzevedo@users.noreply.github.com>
2021-02-12 08:53:08 -0800

    initial commit

--- src/UseEffect/Examples/ExampleTwo.js
+++ src/UseEffect/Examples/ExampleTwo.js
@@ -5,22 +5,23 @@ import ColorButton from "../../components/ColorButton";
 
 import emitt from "../../helpers/emitt";
 import logger from "../../helpers/logger";
-import useRerender from "../../hooks/useRerender";
 
 const emitter = emitt();
 const eventName = "@useEffect:sub";
 
-function Subscription() {
+function Subscription({ count }) {
   const [latestEventValue, setLatestEventValue] = React.useState(null);
   const allSubscriptionsToEvent = emitter.all().get(eventName);
 
   React.useEffect(() => {
     const sub = emitter.on(eventName, (value) => {
-      logger.purpleLogger(`Event(${eventName}) with value: ${value}`);
+      logger.purpleLogger(
+        `Event(${eventName}) with value: ${value} and count: ${count}`
+      );
       setLatestEventValue(value);
     });
     return () => sub.off();
-  });
+  }, [count]);
 
   return (
     <ColorBox variant="blue">
@@ -28,13 +29,13 @@ function Subscription() {
         Event({eventName})
       </Heading>
       <Text>{latestEventValue || "No event emitted yet."}</Text>
-      <Text>Number of subscriptions: {allSubscriptionsToEvent?.size || 0}</Text>
+      <Text>Number of subscriptions: {allSubscriptionsToEvent?.size || 1}</Text>
     </ColorBox>
   );
 }
 
 export default function UseEffectExampleTwo() {
-  const rerender = useRerender();
+  const [count, increment] = React.useReducer((counter) => counter + 1, 0);
 
   return (
     <>
@@ -42,7 +43,7 @@ export default function UseEffectExampleTwo() {
         Main case Two - Subscriptions
       </Heading>
       <Text>Comment the clean-up function and press rerender.</Text>
-      <ColorButton margin="10px" variant="purple" onClick={rerender}>
+      <ColorButton margin="10px" variant="purple" onClick={increment}>
         Rerender
       </ColorButton>
       <ColorButton
@@ -52,7 +53,7 @@ export default function UseEffectExampleTwo() {
       >
         Emit Event
       </ColorButton>
-      <Subscription />
+      <Subscription count={count} />
     </>
   );
 }
